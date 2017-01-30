777c98c09b2f1786727bd82a4e60e6d4b94fd5b2
Sytsmaa <sytsmaa@yahoo.com>
2017-01-30 11:45:10 -0500

    Created PlayerObject Branch
    This branch will deal with any underlying code for the player object.

---
+++ game/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private const int MAX_HIT_POINTS = 100;   //the maximum number of hit points
+    private const int MAX_MANA_POINTS = 100;  //the maximum number of mana points
+
+	/**
+     * Use this for initialization
+     */
+	void Start()
+    {
+		
+	}   //end of Start method
+
+    /** 
+     * Update is called once per frame
+     */
+    void Update()
+    {
+		
+	}   //end of Update method
+}   //end of Player class
