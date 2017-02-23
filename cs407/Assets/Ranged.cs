e3703e3d3cfe5c7278aaef85fb008925afb9aaa8
meyer104 <meyer104@purdue.edu>
2017-02-23 17:32:50 -0500

    Finished most other features

---
+++ cs407/Assets/Ranged.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranged : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Out of Bonds Collider")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+	}
+    void LateUpdate() {
+        transform.parent = null;
+    }
+}
