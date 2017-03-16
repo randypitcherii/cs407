169ffc27e0a0d95f69afa5ebbeaf4102d95366df
Randy <rpitcher@purdue.edu>
2017-03-16 16:40:02 -0400

    Added AI Visual Studio Project

---
+++ NEAT_Workspace/AI/AI/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    class GameState
+    {
+        //get game state matrix
+        public int[] GetCurrentState()
+        {
+            //add logic to query game state later, static for now
+            return new int[4] {1,2,1,1};
+        }
+    }
+}
---
+++ NEAT_Workspace/AI/AI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpNeat.Core;
+
+namespace AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //run main program
+            Console.WriteLine("Looks like all libraries are loading properly.");
+        }
+    }
+}
