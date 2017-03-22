52c06545a8d1cebfa51a2f72b68ac40f4d313357
Sytsmaa <sytsmaa@yahoo.com>
2017-03-22 13:28:45 -0400

    Updated PlayerAI.cs
    Updated PlayerAI.cs
    - Has less pseudocode
    - Added performAction(int) method which actual performs the action
    - Added a threshold constant to know when to perform an action

--- cs407/Assets/scripts/player/AI/PlayerAI.cs
+++ cs407/Assets/scripts/player/AI/PlayerAI.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerAI : Player
 {
-    private AILogger log = null;    //logs the AI's actions
+    //constants
+    private const double ACTION_THRESHOLD = 0.5;    //the threshold to determine whether or not an action should be performed
+
+    //private fields
+    private AILogger log = null;                    //logs the AI's actions
 
     /**
      * Handles the player's movement.
@@ -12,33 +16,54 @@ public class PlayerAI : Player
     public override void LateUpdate()
     {
         /*
-        DATA_TYPE action = ML_FUNC(DATA_ARRAY); //the action the AI should perform
+        ISignalArray actions = new AIFighter(BRAIN).GetMoves(Location_Script.buildArray()); //the actions the AI should perform
 
+        //perfrom the actions that pass the specified threshold
+        for (int actionIndex = 0; actionIndex < actions.Length; actionIndex += 1)
+        {
+            //check if the actiono should be performed
+            if (actions[actionIndex] >= ACTION_THRESHOLD)   //the action should be performed
+            {
+                //perform the action
+                performAction(actionIndex);
+            }   //end if
+        }   //end for
+        */
+    }   //end of LateUpdate method
+
+    /**
+     * Performs the given action.
+     *
+     * @action  The action to perform.
+     */
+    public void performAction(int action)
+    {
+        /*
         //check what the AI should do
         switch (action)
         {
-        case DO_NOTHING:    //the AI should do nothing
-            break;
-        case MOVE_LEFT: //the AI should move left
-            moveLeft();
-            break;
-        case MOVE_RIGHT:    //the AI should move right
-            moveRight();
-            break;
-        case JUMP:  //the AI should jump
-            jump();
-            break;
-        case MELEE_ATTACK:  //the AI should perform a melee attack
-            useMeleeAttack();
-            break;
-        case RANGED_ATTACK: //the AI should perform a ranged attack
-            useRangedAttack();
-            break;
-        case BLOCK_ATTACK:  //the AI should perform a block attack
-            useBlockAttack();
-            break;
-        default:    //error
-            Debug.LogError("action=" + action);
+            case DO_NOTHING:    //the AI should do nothing
+                break;
+            case MOVE_LEFT: //the AI should move left
+                moveLeft();
+                break;
+            case MOVE_RIGHT:    //the AI should move right
+                moveRight();
+                break;
+            case JUMP:  //the AI should jump
+                jump();
+                break;
+            case MELEE_ATTACK:  //the AI should perform a melee attack
+                useMeleeAttack();
+                break;
+            case RANGED_ATTACK: //the AI should perform a ranged attack
+                useRangedAttack();
+                break;
+            case BLOCK_ATTACK:  //the AI should perform a block attack
+                useBlockAttack();
+                break;
+            default:    //error
+                Debug.LogError("action=" + action);
         }   //end switch
 
         //check if a log was created
@@ -50,7 +75,7 @@ public class PlayerAI : Player
         //write the action to the log
         this.log.write(action);
         */
-    }   //end of LateUpdate method
+    }   //end of performAction method
 
     /**
      * Closes the AI's logger.
