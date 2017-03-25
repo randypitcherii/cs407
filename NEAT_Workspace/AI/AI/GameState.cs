5b6b8cd775967fb9c4180975ad3851352902326f
Randy <rpitcher@purdue.edu>
2017-03-25 13:11:58 -0400

    Consolidated / simplified game state constants

--- NEAT_Workspace/AI/AI/GameState.cs
+++ NEAT_Workspace/AI/AI/GameState.cs
@@ -8,13 +8,27 @@ namespace AI
 {
     class GameState
     {
-        public static int aiInputMatrixSize = 100;//change this later
-        public static int aiOutputMatrixSize = 100;//change this later too
-        //get game state matrix
-        public int[] GetCurrentState()
-        {
-            //add logic to query game state later, static for now
-            return new int[4] {1,2,1,1};
-        }
+        public const int aiInputMatrixSize = 100;//change this later
+        public const int aiOutputMatrixSize = 100;//change this later too
+
+        //=====================================================================
+        //states for AI input matrix
+        //=====================================================================
+        //environment positions
+        public const int nothing_state = 0;
+        public const int ground_state = 1;
+
+        //character positions
+        public const int self_state = 5;
+        public const int opponent_state = 10;
+
+        //missile positions
+        public const int selfRangedAttack_state = 0;
+        public const int opponentRangedAttack_state = 20;
+
+        //non-positional game state
+        public const int isBlocking_state = 1;
+        public const int isFiring_state = 1;
+        //=====================================================================
     }
 }
