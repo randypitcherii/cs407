df41c6d879ca5e87819c6e6f997f61583be78f48
Randy <rpitcher@purdue.edu>
2017-03-26 19:51:30 -0400

    Upgraded to 4.x .NET Unity Version
    All compiles, now need to implement automatic gameplay

---
+++ cs407/Assets/scripts/AI/AIFighterEvaluatorFactory.cs
@@ -0,0 +1,17 @@
+﻿using AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpNeat.Phenomes;
+
+namespace Assets.scripts.AI
+{
+    class AIFighterEvaluatorFactory : IEvaluatorFactory
+    {
+        public ICoevolutionPhenomeEvaluator<IBlackBox> getNewEvaluator()
+        {
+            return new AIEvaluator();
+        }
+    }
+}
