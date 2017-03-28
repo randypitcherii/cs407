461c63422e8ae315f7fd3dd7fa84c106431f5260
Randy <rpitcher@purdue.edu>
2017-03-27 20:35:36 -0400

    Put AI decisions in AIBrain class

--- cs407/Assets/scripts/player/AI/PlayerAI.cs
+++ cs407/Assets/scripts/player/AI/PlayerAI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.scripts.player.AI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,21 +14,7 @@ public class PlayerAI : Player
     public override void LateUpdate()
     {
 
-        System.Random r = new System.Random();
-
-        //create random actions
-        int[] actions = { 0, 0, 0, 0, 0, 0, 0 };
-
-        if (r.Next(0, 50) == 1)
-        {
-            actions[0] = r.Next(0, 5); //left key pressed
-            actions[1] = r.Next(0, 5); //right key pressed
-            actions[2] = r.Next(0, 2); //jump key pressed
-            actions[3] = r.Next(0, 2); //melee attack key pressed
-            actions[4] = r.Next(0, 2); //ranged attack key pressed
-            actions[5] = r.Next(0, 2); //block key pressed
-            actions[6] = r.Next(0, 1); //stand still
-        }
+        int [] actions = AIBrain.getMoves(ls.buildArray());
 
         //check if a key was pressed
         if (actions[0] != 0)   //a left key was pressed
