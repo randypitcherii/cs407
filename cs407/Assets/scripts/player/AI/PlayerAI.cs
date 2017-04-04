03facc81c047c6c6f80a64e9d8d27d3048154195
Sytsmaa <sytsmaa@yahoo.com>
2017-04-04 13:25:57 -0400

    PlayerAI set to new changes again

--- cs407/Assets/scripts/player/AI/PlayerAI.cs
+++ cs407/Assets/scripts/player/AI/PlayerAI.cs
@@ -1,79 +1,62 @@
-﻿using System.Collections;
+﻿using Assets.scripts.player.AI;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAI : Player
 {
-    //constants
-    private const double ACTION_THRESHOLD = 0.5;    //the threshold to determine whether or not an action should be performed
-
-    //private fields
-    private AILogger log = null;                    //logs the AI's actions
+    private AILogger log = null;    //logs the AI's actions
 
     /**
      * Handles the player's movement.
      */
     public override void LateUpdate()
     {
-        /*
-        ISignalArray actions = new AIFighter(BRAIN).GetMoves(Location_Script.buildArray()); //the actions the AI should perform
 
-        //perfrom the actions that pass the specified threshold
-        for (int actionIndex = 0; actionIndex < actions.Length; actionIndex += 1)
-        {
-            //check if the actiono should be performed
-            if (actions[actionIndex] >= ACTION_THRESHOLD)   //the action should be performed
-            {
-                //perform the action
-                performAction(actionIndex);
-            }   //end if
-        }   //end for
-        */
-    }   //end of LateUpdate method
+        int[] actions = AIBrain.getMoves(ls.buildArray());
 
-    /**
-     * Performs the given action.
-     *
-     * @action  The action to perform.
-     */
-    public void performAction(int action)
-    {
-        /*
-        //check what the AI should do
-        switch (action)
+        //check if a key was pressed
+        if (actions[0] != 0)   //a left key was pressed
         {
-            case DO_NOTHING:    //the AI should do nothing
-                break;
-            case MOVE_LEFT: //the AI should move left
-                moveLeft();
-                break;
-            case MOVE_RIGHT:    //the AI should move right
-                moveRight();
-                break;
-            case JUMP:  //the AI should jump
-                jump();
-                break;
-            case MELEE_ATTACK:  //the AI should perform a melee attack
-                useMeleeAttack();
-                break;
-            case RANGED_ATTACK: //the AI should perform a ranged attack
-                useRangedAttack();
-                break;
-            case BLOCK_ATTACK:  //the AI should perform a block attack
-                useBlockAttack();
-                break;
-            default:    //error
-                Debug.LogError("action=" + action);
-        }   //end switch
-        //check if a log was created
-        if (this.log == null)   //a log was not created
+            moveLeft();
+        }
+        else if (actions[1] != 0) //a right key was pressed
         {
-            this.log = new AILogger();
+            moveRight();
+        }
+        else if (actions[6] == 1)    //no key was pressed
+        {
+            standStill();
         }   //end if
-        //write the action to the log
-        this.log.write(action);
-        */
-    }   //end of performAction method
+
+
+        if (actions[2] == 1) //a jump key was pressed
+        {
+            jump();
+        }
+        else if (actions[3] == 1) //a melee attack key was pressed
+        {
+            useMeleeAttack();
+        }
+        else if (actions[4] == 1)    //a ranged attack key was pressed
+        {
+            useRangedAttack();
+        }
+        else if (actions[5] == 1) //a block attack key was pressed
+        {
+            useBlockAttack();
+        }
+
+        if (actions[3] == 0)
+        {
+            endMeleeAttack();
+        }
+        if (actions[5] != 1)
+        {
+            endBlockAttack();
+        }
+    }   //end of LateUpdate method
 
     /**
      * Closes the AI's logger.
