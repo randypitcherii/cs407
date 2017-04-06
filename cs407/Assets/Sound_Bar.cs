108c00494e878166b47ba9e34fd2f33595144f17
meyer104 <meyer104@purdue.edu>
2017-04-06 17:52:22 -0400

    Add volume controls

---
+++ cs407/Assets/Sound_Bar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Sound_Bar : MonoBehaviour {
+    public float soundLevel;
+    private Slider s;
+    public void onLoad()
+    {
+        //TODO make way to set soundLevel from file instead of this
+        soundLevel = .30f;
+        s = this.GetComponent<Slider>();
+        s.value = soundLevel;
+    }
+    public void onDeLoad()
+    {
+        s = this.GetComponent<Slider>();
+        soundLevel = s.value;
+        Debug.Log(soundLevel);
+        //TODO save value were ever
+    }
+    // Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
